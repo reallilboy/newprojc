89f5eab14f9c7d353b0970d50c1a6d2cddfdc4de
reallilboy <reallilboy696@gmail.com>
2025-03-01 10:06:45 +0330

    add register models for admin

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Products,Category,Order,Customer
 # Register your models here.
+
+admin.site.register(Products)
+admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Order)
