214ad46df6d3536cce71ec78c939d3e02f424d76
reallilboy <reallilboy696@gmail.com>
2025-03-01 17:26:16 +0330

    create login and logout for user

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from . import views as shop_views
 urlpatterns = [
-        path('',shop_views.home,name='home')
+        path('',shop_views.home,name='home'),
+        path('login_user/',shop_views.login_user,name='login_user'),
+        path('logout/',shop_views.logout_user,name='logout_user'),
 ]
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Products,Category
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+
+
 # Create your views here.
+
 def home(request):
         products = Products.objects.all()
         return render(request,'index.html',{'products':products})
+
+def login_user(request):
+        if request.method == 'POST':
+                username = request.POST['username']
+                password = request.POST['password']
+                user = authenticate(request,username=username,password=password)
+                if user is not None:
+                        login(request,user)
+                        messages.success(request,('success'))
+                        return redirect('home')
+                else:
+                        messages.success(request,('you have error'))
+                        return redirect('login_user')
+        else:
+                return render(request,'login_user.html')
+
+
+def logout_user(request):
+        logout(request)
+        return redirect('home')
