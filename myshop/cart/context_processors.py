818e63f4b6979599d77560dd73456bb3de3fb7c7
reallilboy <reallilboy696@gmail.com>
2025-03-03 10:33:16 +0330

    cretae cart and shoping

--- myshop/cart/context_processors.py
+++ myshop/cart/context_processors.py
@@ -3,3 +3,11 @@ from .cart import Cart
 
 def cart (request):
         return {'cart':Cart(request)}
+
+def cart_item_count(request):
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user).first()
+        cart_item_count = cart.items.count() if cart else 0
+    else:
+        cart_item_count = 0
+    return {'cart_item_count': cart_item_count}
--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -5,7 +5,6 @@ from django.http import JsonResponse
 
 
 
-# Create your views here.
 def cart_summary(request):
         return render(request,'cart_summary.html',{})
 
@@ -13,13 +12,10 @@ def cart_summary(request):
 def cart_add(request):
         cart = Cart(request)
         if request.POST.get('action') == 'post':
-                product_id = int(request.POST.get('product_id'))
-                product = get_object_or_404(Products,id=product_id)
+                products_id = int(request.POST.get('product_id'))
+                product = get_object_or_404(Products,id=products_id)
                 cart.add(product=product)
-                response = JsonResponse({'product name':product.name})
-                return response
-
-
+                return JsonResponse({ 'product name': product.product_name})
 
 def cart_delete(request):
         pass
